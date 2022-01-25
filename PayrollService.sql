1ba14379ae9b15598653b84dee1c43c85a5f02cb
ErElectical <mohittanwar786.mt@gmail.com>
2022-01-25 08:06:26 +0000

    Payroll Service Database

---
+++ PayrollService.sql
@@ -0,0 +1,17 @@
+create database Payroll_Services;
+
+/* Syntax to create a database is --- create database [databaseName]; */
+
+/* after creating database we have to move in database for which we use syntax -- USE [database]; */
+
+USE Payroll_Services;
+
+
+
+
+
+
+
+
+
+
