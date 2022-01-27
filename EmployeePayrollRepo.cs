3d1e976cafeda8352f55f9ce9c0ec0acdd565091
Mohit <94623283+ErElectrical@users.noreply.github.com>
2022-01-27 17:10:50 +0000

    Added: Repo class Modifications

--- EmployeePayrollRepo.cs
+++ EmployeePayrollRepo.cs
@@ -9,8 +9,11 @@ namespace Employee_Payroll
 {
     public class EmployeePayrollRepo
     {
-        public static string ConnectionString = @"Server=DESKTOP-2G853OJ;Initial Catalog=Employee_Pay;Integrated Security=True";
+        public static string ConnectionString = @"DataServer=DESKTOP-2G853OJ;Initial Catalog=Payroll_Services;Integrated Security=True";
         SqlConnection connection = new SqlConnection(ConnectionString);
+
+
+
         public void GetAllData()
         {
             try
@@ -27,29 +30,31 @@ namespace Employee_Payroll
                     this.connection.Open();
                     SqlDataReader dr = cmd.ExecuteReader();
 
-                    if(dr.HasRows)
+                    if (dr.HasRows)
                     {
-                        while(dr.Read())
+                        while (dr.Read())
                         {
-                            EmployeePayrollModel.EmployeeId = dr.GetInt32(0);
-                            EmployeePayrollModel.stratdate = dr.GetDateTime(1);
-                            EmployeePayrollModel.Phonenumber = dr.GetString(2);
-                            EmployeePayrollModel.Address = dr.GetString(3);
-                            EmployeePayrollModel.Department = dr.GetString(4);
-                            EmployeePayrollModel.state = dr.GetString(5);
-                            EmployeePayrollModel.Country = dr.GetString(6);
-                            EmployeePayrollModel.basicPay = dr.GetDouble(7);
-                            EmployeePayrollModel.Deducations = dr.GetDouble(8);
-       
-                            EmployeePayrollModel.TaxPay = dr.GetDouble(9);
-                            EmployeePayrollModel.Tax = dr.GetDouble(10);
+                            model.EmployeeId = dr.GetInt32(0);
+                            model.stratdate = dr.GetDateTime(1);
+                            model.Phonenumber = dr.GetString(2);
+                            model.Address = dr.GetString(3);
+                            model.Department = dr.GetString(4);
+                            model.state = dr.GetString(5);
+                            model.Country = dr.GetString(6);
+                            model.basicPay = dr.GetDouble(7);
+                            model.Deducations = dr.GetDouble(8);
 
-                            EmployeePayrollModel.NetPay = dr.GetDouble(11);
+                            model.Deducations = dr.GetDouble(8);
 
-                            EmployeePayrollModel.gender = dr.GetString(12);
-                            EmployeePayrollModel.EmployeeName = dr.GetString(13);
+                            model.TaxPay = dr.GetDouble(9);
+                            model.Tax = dr.GetDouble(10);
 
-                            Console.WriteLine("{0}{1}{2}{3}{4}{5}{6}", EmployeePayrollModel.EmployeeId, EmployeePayrollModel.EmployeeName, EmployeePayrollModel.stratdate, EmployeePayrollModel.Phonenumber, EmployeePayrollModel.NetPay, EmployeePayrollModel.state, EmployeePayrollModel.TaxPay);
+                            model.NetPay = dr.GetDouble(11);
+
+                            model.gender = dr.GetString(12);
+                            model.EmployeeName = dr.GetString(13);
+
+                            Console.WriteLine("{0}{1}{2}{3}{4}{5}{6}", model.EmployeeId, model.EmployeeName, model.stratdate, model.Phonenumber, model.NetPay, model.state, model.TaxPay);
                             Console.WriteLine("\n");
 
 
@@ -77,15 +82,91 @@ namespace Employee_Payroll
 
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
             finally
             {
-                
+
                 this.connection.Close();
             }
+        }
+
+        public void  AddEmployee(EmployeePayrollModel emp)
+        {
+            try
+            {
+                /* this approach is through procedure of sql
+                 * with the help of proceure we can reuse the instructions again and again
+                
+                using(this.connection)
+                {
+                    SqlCommand cmd = new SqlCommand(SPhelpInserting, this.connection);
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue(@EmployeeName, EmployeePayrollModel.EmployeeName);
+                    cmd.Parameters.AddWithValue(@Phonenumber, EmployeePayrollModel.Phonenumber);
+                    cmd.Parameters.AddWithValue(@Startdate, EmployeePayrollModel.stratdate);
+
+                    cmd.Parameters.AddWithValue(@Emp_Address, EmployeePayrollModel.Address);
+
+                    cmd.Parameters.AddWithValue(@Dept_Name, EmployeePayrollModel.Department);
+
+                    cmd.Parameters.AddWithValue(@state_belong, EmployeePayrollModel.state);
+
+                    cmd.Parameters.AddWithValue(@Country, EmployeePayrollModel.Country);
+
+                    cmd.Parameters.AddWithValue(@BasicPay, EmployeePayrollModel.basicPay);
+
+                    cmd.Parameters.AddWithValue(@Deducations, EmployeePayrollModel.Deducations);
+
+                    cmd.Parameters.AddWithValue(@TaxPay, EmployeePayrollModel.TaxPay);
+
+                    cmd.Parameters.AddWithValue(@IncomeTax, EmployeePayrollModel.Tax);
+
+                    cmd.Parameters.AddWithValue(@NetPay, EmployeePayrollModel.NetPay);
+
+                    cmd.Parameters.AddWithValue(@gender, EmployeePayrollModel.gender);
+                */
+
+
+                using (this.connection)
+                {
+                    string query = @"insert into Employee_Pay(Startdate,Emp_PhoneNumber,Emp_Address,Dept_Name,state_belong,Country,BasicPay,Deducations,TaxPay,IncomeTax,NetPay,gender,Emp_Name" +
+                                    "values(emp.stratdate,emp.Phonenumber,emp.Address,emp.Department,emp.state,emp.Country,emp.basicPay,emp.Deducations,emp.TaxPay,emp.Tax,emp.gender,emp.EmployeeName)";
+
+                    this.connection.Open();
+                    SqlCommand cmd = new SqlCommand(query, this.connection);
+                    this.connection.Close();
+                    int numberofinsertion = cmd.ExecuteNonQuery();
+
+                    if (numberofinsertion > 0)
+                    {
+                        Console.WriteLine("insertion occur");
+                    }
+                    
+
+
+
+
+
+
+
+
+                }
+              
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+
         }
     }
 }
