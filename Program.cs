370315d3998a0c74fbd5e1596614a90350b090ca
Mohit <94623283+ErElectrical@users.noreply.github.com>
2022-01-27 10:17:30 +0000

    Added: Data to Model class

--- Program.cs
+++ Program.cs
@@ -9,6 +9,25 @@ namespace Employee_Payroll
             Console.WriteLine("Welcome to ADO.Net Tutorial");
             EmployeePayrollRepo emp = new EmployeePayrollRepo();
             emp.GetAllData();
+
+            EmployeePayrollModel temp = new EmployeePayrollModel();
+
+            EmployeePayrollModel.EmployeeName = "moke";
+            EmployeePayrollModel.Country = "india";
+            EmployeePayrollModel.state = "maharastra";
+            EmployeePayrollModel.stratdate = DateTime.Today;
+            EmployeePayrollModel.Department = "engg";
+            EmployeePayrollModel.gender = "male";
+            EmployeePayrollModel.Address = "Junu okhkla";
+            EmployeePayrollModel.Tax = 129;
+            EmployeePayrollModel.basicPay = 12000;
+            EmployeePayrollModel.TaxPay = 100;
+            EmployeePayrollModel.NetPay = 11500;
+            EmployeePayrollModel.Deducations = 150;
+            EmployeePayrollModel.Phonenumber = "356642235";
+
+            emp.AddEmployee( temp);
+
         }
     }
 }
