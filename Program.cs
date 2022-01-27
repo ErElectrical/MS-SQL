344f139ba7ce515a9f13453b305b30bd1de1b4c3
Mohit <94623283+ErElectrical@users.noreply.github.com>
2022-01-27 08:53:07 +0000

    Added: Main class Ado.net

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Employee_Payroll
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to ADO.Net Tutorial");
+            EmployeePayrollRepo emp = new EmployeePayrollRepo();
+            emp.GetAllData();
+        }
+    }
+}
